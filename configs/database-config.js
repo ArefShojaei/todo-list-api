c8564531e009eea5b6e50792d3745f5926ae046a
ArefShojaei <arefshojaei82dev@gmail.com>
2023-06-12 14:23:16 +0330

    add mongodb config

--- configs/database-config.js
+++ configs/database-config.js
@@ -1,1 +1,9 @@
-module.exports = {}
+const { MONGODB_HOST, MONGODB_PORT, MONGODB_DB } = process.env
+
+module.exports = {
+    mongodb : {
+        host : MONGODB_HOST,
+        port : MONGODB_PORT,
+        db : MONGODB_DB,
+    }
+}
