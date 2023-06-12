ab8df690ab64ce79b3a25381c00b48032379e6b9
ArefShojaei <arefshojaei82dev@gmail.com>
2023-06-12 14:23:38 +0330

    add mongodb database handler

--- databases/mongodb-database.js
+++ databases/mongodb-database.js
@@ -1,1 +1,16 @@
-module.exports = async () => {}
+const mongoose = require("mongoose");
+const {
+    mongodb: { db, host, port },
+} = require("@configs/database-config");
+
+module.exports = async () => {
+    try {
+        const connection = await mongoose.connect(`mongodb://${host}:${port}/${db}`)
+        console.log("Database was connected successfully.");
+
+
+        return connection
+    } catch (err) {
+        console.log(`Database was not connected! - ${err}`);
+    }
+};
