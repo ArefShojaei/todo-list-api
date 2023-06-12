e91d5fed467a08221da73f8888e3f188565a0a28
ArefShojaei <arefshojaei82dev@gmail.com>
2023-06-12 13:37:17 +0330

    start project

---
+++ app/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { APP_PORT } = process.env
+const app = express()
+const port = APP_PORT || 3000
+const cors = require('cors');
+const notFoundRoute = require('@middlewares/404-middleware');
+const todoRoute = require('@routes/todo-route');
+
+module.exports = () => {
+    // init cors config
+    app.use(cors())
+
+    // init routes
+    app.use("/api/todos", todoRoute)
+    app.use(notFoundRoute)
+
+    // launch the app
+    app.listen(port, () => console.log(`App is running on port ${port}...`))
+}
