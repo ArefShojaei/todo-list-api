7c19090b43a522d7f78c176e0e644af36db33199
ArefShojaei <arefshojaei82dev@gmail.com>
2023-06-12 14:59:01 +0330

    add updateById todo function

--- app/controllers/todo-controller.js
+++ app/controllers/todo-controller.js
@@ -41,7 +41,27 @@ exports.getById = async (req, res) => {
     }
 }
 
-exports.updateById = async (req, res) => {}
+exports.updateById = async (req, res) => {
+    try {
+        const { id } = req.params
+        
+        const updatedTodo = await TodoModel.updateOne({ _id : id }, { ...req.body })
+
+
+        return res.status(200).json({
+            status : "Success",
+            code : 200,
+            message : "Todo was gotten.",
+            todo : updatedTodo,
+        }) 
+    } catch (err) {
+        return res.status(500).json({
+            status : "Error",
+            code : 500,
+            message : "Request was failed"
+        })
+    }
+}
 
 exports.deleteAll = async (req, res) => {}
 
