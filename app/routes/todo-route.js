cc09115db7bf34945bbd5efd9869305c02878eed
ArefShojaei <arefshojaei82dev@gmail.com>
2023-06-12 14:40:16 +0330

    add todo routes + controllers

--- app/routes/todo-route.js
+++ app/routes/todo-route.js
@@ -1,5 +1,18 @@
 const { Router } = require("express");
 const router = Router()
+const {
+    getAll,
+    getById,
+    updateById,
+    deleteAll,
+    deleteById
+} = require('../controllers/todo-controller');
 
+router
+    .get("/", getAll)
+    .get("/:id", getById)
+    .patch("/:id", updateById)
+    .delete("/", deleteAll)
+    .delete("/:id", deleteById)
 
 module.exports = router
